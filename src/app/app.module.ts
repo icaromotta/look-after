6ff486e68ef4e95d94e2bce91b2229f194dccb1b
Ícaro Costa <icarocmotta@gmail.com>
2020-02-07 16:11:15 -0300

    partial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { InfoComponent } from './pages/info/info.component';
+import { CarouselComponent } from './shared/carousel/carousel.component';
+import { CardComponent } from './shared/card/card.component';
+import { RegisterComponent } from './shared/forms/register/register.component';
+import { LoginComponent } from './shared/forms/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    NavbarComponent
+    NavbarComponent,
+    InfoComponent,
+    CarouselComponent,
+    CardComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
