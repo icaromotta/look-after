7982936f9332faf697d83fcdbdf933162825154e
Ícaro Costa <icarocmotta@gmail.com>
2020-02-11 14:58:53 -0300

    json to carousel

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
+  public diapers = [{
+    brand: 'ipsum',
+    size: 'P',
+    image: '../../../assets/fralda_p.png'
+  }, {
+    brand: 'consectetur',
+    size: 'M',
+    image: '../../../assets/fralda_m.png'
+  }, {
+    brand: 'adipiscing',
+    size: 'G',
+    image: '../../../assets/fralda_g.png'
+  }, {
+    brand: 'mollit',
+    size: 'GG',
+    image: '../../../assets/fralda_gg.png'
+  }]
+  public currentImage: string
+
   constructor() { }
 
   ngOnInit() {
+    this.diapers
+    this.rotate()
   }
+
+  rotate() {
+
+    // let count = 0
+
+    // setInterval(() => {
+
+    //   console.log(this.diapers[count++])
+
+    //   if(count >= this.diapers.length) {
+    //     count = 0
+    //   }
+    // }, 2000);
+  }
+
 
 }
